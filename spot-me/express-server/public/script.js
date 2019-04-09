e5df0d836ca28b904ccfc599f4eb11be33523997
Lora <jonathan.lora.95@gmail.com>
2019-04-09 09:58:06 -0400

    refactored code for the first container and built simple functionality for switching a div s content

--- spot-me/express-server/public/script.js
+++ spot-me/express-server/public/script.js
@@ -4,98 +4,143 @@
 
 /* FIRST CONTAINER */
 
-const first = document.querySelector('#first');
-first.style.width = '100vw';
-first.style.height = '40vh';
-first.style.display = 'flex';
-first.style.flexDirection = 'row';
-first.style.justifyContent = 'space-around';
+// Rendering AND Styling
+const renderFirst = () => {
+  const first = document.querySelector('#first');
+  first.style.width = '100vw';
+  first.style.height = '40vh';
+  first.style.display = 'flex';
+  first.style.flexDirection = 'row';
+  first.style.justifyContent = 'space-around';
+};
+renderFirst();
 // Adding divs for the logo, slogan, and an about-me
-const logo = document.createElement('div');
-const slogan = document.createElement('div');
-const about = document.createElement('div');
-const firstArray = [logo, slogan, about];
-firstArray.forEach(el => first.appendChild(el));
-// logoDiv.style.backgroundImage = 'url(./images/logo.png)';
-firstArray[0].style.backgroundColor = 'lightcoral';
-firstArray[0].style.width = '30%';
-firstArray[0].style.height = '40vh';
-firstArray[1].style.backgroundColor = 'lightslategray';
-firstArray[1].style.width = '30%';
-firstArray[1].style.height = '40vh';
-firstArray[2].style.backgroundColor = 'lightpink';
-firstArray[2].style.width = '30%';
-firstArray[2].style.height = '40vh';
+const renderFirstChildren = () => {
+  const first = document.querySelector('#first');
+  const logo = document.createElement('div');
+  const slogan = document.createElement('div');
+  const firstArray = [logo, slogan];
+  firstArray.forEach(el => first.appendChild(el));
+  // logoDiv.style.backgroundImage = 'url(./images/logo.png)';
+  // Giving all divs in First Container a width and a height
+  firstArray.forEach((el) => {
+    const divs = el;
+    divs.style.width = '30%';
+    divs.style.height = '40vh';
+  });
+  firstArray[0].style.backgroundColor = 'lightcoral';
+  firstArray[1].style.backgroundColor = 'lightslategray';
+};
+renderFirstChildren();
+
+// Functionality
+// Allows events to be chained to one element
+EventTarget.prototype.addEventListener = (() => {
+  const { addEventListener } = EventTarget.prototype;
+  return function el() {
+    // this refers to the element
+    // this also loses its context inside an arrow funtcion
+    // eslint-disable-next-line prefer-rest-params
+    addEventListener.apply(this, arguments);
+    return this;
+  };
+})();
+// Show/hide between the slogan and about
+const toggle = () => {
+  const slogan = document.querySelector('#first').lastChild;
+  const button = document.createElement('button');
+  // Toggle slogan/about text
+  slogan.addEventListener('mouseenter', () => {
+    slogan.style.backgroundColor = 'blue';
+    slogan.appendChild(button);
+    // Seamless scroll to interactive slides
+    button.addEventListener('click', () => {
+      console.log('click');
+    });
+  }).addEventListener('mouseleave', () => {
+    slogan.style.backgroundColor = 'lightslategray';
+    slogan.removeChild(button);
+  });
+};
+toggle();
 
 /* SECOND CONTAINER */
-
-const second = document.querySelector('#second');
-second.style.backgroundcolor = 'black';
-second.style.width = '100vw';
-second.style.height = '80vh';
-second.style.display = 'flex';
+const renderSecond = () => {
+  const second = document.querySelector('#second');
+  second.style.backgroundcolor = 'black';
+  second.style.width = '100vw';
+  second.style.height = '80vh';
+  second.style.display = 'flex';
+};
+renderSecond();
 // Second container will serve two interactive slides
-const secondOne = document.createElement('div');
-const secondTwo = document.createElement('div');
-const secondArray = [secondOne, secondTwo];
-secondArray.forEach(el => second.appendChild(el));
-// FIRST SLIDE
-secondArray[0].style.flex = '1';
-secondArray[0].style.backgroundColor = 'yellow';
-secondArray[0].style.display = 'flex';
-secondArray[0].style.flexDirection = 'row';
-secondArray[0].style.justifyContent = 'center';
-secondArray[0].style.justifyContent = 'space-evenly';
-secondArray[0].style.flexWrap = 'wrap';
-// Adding text and two clickable divs/buttons
-const becomeText = document.createElement('div');
-const borrower = document.createElement('div');
-const lender = document.createElement('div');
-const secondOneArray = [becomeText, borrower, lender];
-secondOneArray.forEach(el => secondArray[0].appendChild(el));
-secondOneArray[0].style.width = '75%';
-secondOneArray[0].style.height = '10vh';
-secondOneArray[0].style.backgroundColor = 'red';
-secondOneArray[1].style.width = '30%';
-secondOneArray[1].style.height = '60vh';
-secondOneArray[1].style.backgroundColor = 'blue';
-secondOneArray[2].style.width = '30%';
-secondOneArray[2].style.height = '60vh';
-secondOneArray[2].style.backgroundColor = 'green';
-// SECOND SLIDE
-secondArray[1].style.flex = '1';
-secondArray[1].style.backgroundColor = 'violet';
-secondArray[1].style.display = 'none';
-secondArray[1].style.flexDirection = 'column';
-secondArray[1].style.alignItems = 'center';
-secondArray[1].style.alignItems = 'space-evenly';
-// Adding text/question, input field, submit button, and three lines for the results
-const howMuchText = document.createElement('div');
-const numInput = document.createElement('input');
-numInput.type = 'number';
-numInput.placeholder = 'Enter amount';
-const button = document.createElement('button');
-const oneWeek = document.createElement('p');
-oneWeek.textContent = 'Week 1:';
-const twoWeek = document.createElement('p');
-twoWeek.textContent = 'Week 2:';
-const oneMonth = document.createElement('p');
-oneMonth.textContent = 'First Month:';
-const secondTwoArray = [howMuchText, numInput, button, oneWeek, twoWeek, oneMonth];
-secondTwoArray.forEach(el => secondArray[1].appendChild(el));
-secondTwoArray[0].style.width = '75%';
-secondTwoArray[0].style.height = '15vh';
-secondTwoArray[0].style.backgroundColor = 'orange';
-secondTwoArray[1].style.width = '25%';
-secondTwoArray[1].style.height = '5vh';
-secondTwoArray[2].style.width = '5%';
-secondTwoArray[2].style.height = '3vh';
-secondTwoArray[3].style.width = '25%';
-secondTwoArray[3].style.height = '10vh';
-secondTwoArray[4].style.width = '25%';
-secondTwoArray[4].style.height = '10vh';
-secondTwoArray[5].style.width = '25%';
-secondTwoArray[5].style.height = '10vh';
+const renderSecondChildren = () => {
+  const second = document.querySelector('#second');
+  const secondOne = document.createElement('div');
+  const secondTwo = document.createElement('div');
+  const secondArray = [secondOne, secondTwo];
+  secondArray.forEach(el => second.appendChild(el));
+  // FIRST SLIDE
+  secondArray[0].style.flex = '1';
+  secondArray[0].style.backgroundColor = 'yellow';
+  secondArray[0].style.display = 'flex';
+  secondArray[0].style.flexDirection = 'row';
+  secondArray[0].style.justifyContent = 'center';
+  secondArray[0].style.justifyContent = 'space-evenly';
+  secondArray[0].style.flexWrap = 'wrap';
+  // Adding text and two clickable divs/buttons
+  const becomeText = document.createElement('div');
+  const borrower = document.createElement('div');
+  const lender = document.createElement('div');
+  const secondOneArray = [becomeText, borrower, lender];
+  secondOneArray.forEach(el => secondArray[0].appendChild(el));
+  secondOneArray.forEach((el) => {
+    const divs = el;
+    divs.style.width = '30%';
+    divs.style.height = '60vh';
+  });
+  secondOneArray[0].style.width = '75%';
+  secondOneArray[0].style.height = '10vh';
+  secondOneArray[0].style.backgroundColor = 'red';
+  secondOneArray[1].style.backgroundColor = 'blue';
+  secondOneArray[2].style.backgroundColor = 'green';
+  // SECOND SLIDE
+  secondArray[1].style.flex = '1';
+  secondArray[1].style.backgroundColor = 'violet';
+  secondArray[1].style.display = 'none';
+  secondArray[1].style.flexDirection = 'column';
+  secondArray[1].style.alignItems = 'center';
+  secondArray[1].style.alignItems = 'space-evenly';
+  // Adding text/question, input field, submit button, and three lines for the results
+  const howMuchText = document.createElement('div');
+  const numInput = document.createElement('input');
+  numInput.type = 'number';
+  numInput.placeholder = 'Enter amount';
+  const button = document.createElement('button');
+  const oneWeek = document.createElement('p');
+  oneWeek.textContent = 'Week 1:';
+  const twoWeek = document.createElement('p');
+  twoWeek.textContent = 'Week 2:';
+  const oneMonth = document.createElement('p');
+  oneMonth.textContent = 'First Month:';
+  const secondTwoArray = [howMuchText, numInput, button, oneWeek, twoWeek, oneMonth];
+  secondTwoArray.forEach(el => secondArray[1].appendChild(el));
+  secondTwoArray.forEach((el) => {
+    const divs = el;
+    divs.style.width = '25%';
+    divs.style.height = '10vh';
+  });
+  secondTwoArray[0].style.width = '75%';
+  secondTwoArray[0].style.height = '15vh';
+  secondTwoArray[0].style.backgroundColor = 'orange';
+  secondTwoArray[1].style.width = '25%';
+  secondTwoArray[1].style.height = '5vh';
+  secondTwoArray[2].style.width = '5%';
+  secondTwoArray[2].style.height = '3vh';
+};
+renderSecondChildren();
+
+// Functionality
 
 /* THIRD CONTAINER */
 
@@ -107,7 +152,7 @@ third.style.display = 'flex';
 third.style.flexDirection = 'row';
 third.style.justifyContent = 'center';
 // Adding three small containers EACH containing
-// small logo, text/title, and description
+// Small logo, text/title, and description
 const thirdArray = [];
 for (let i = 0; i <= 2; i += 1) {
   thirdArray.push(document.createElement('div'));
